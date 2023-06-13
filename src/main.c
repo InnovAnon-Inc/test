658259efd125cbac7c3aa4da93814efbfc811e46
lmaddox <InnovAnon-Inc@gmx.com>
2023-06-13 18:17:49 +0530

    libtool

--- src/main.c
+++ src/main.c
@@ -2,7 +2,7 @@
 #include "foo.h"
 
 int main(const int c, char *const args[]) {
-	const err_t e = foo()
+	const err_t e = foo();
 	error_check(e == EOF) return EXIT_FAILURE;
 	return EXIT_SUCCESS;
 }
